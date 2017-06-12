e6cd66f5f86595c3ed30f32128dc506914187299
Cameron Jenner <cjenner37@gmail.com>
2017-06-12 14:58:59 -0600

    users JSON added

--- index.js
+++ index.js
@@ -1,25 +1,67 @@
 $(document).ready(function () {
 
+// cookie key variables for each user
+	var users = [
+		{
+			'name': 'Cameron',
+			'sugar': 'cam-sugar',
+			'chocolate': 'cam-chocolate',
+			'lemon': 'cam-lemon'
+		},
+		{
+			'name': 'Casey',
+			'sugar': 'casey-sugar',
+			'chocolate': 'casey-chocolate',
+			'lemon': 'casey-lemon'
+		},
+		{
+			'name': '',
+			'sugar': 'sugar',
+			'chocolate': 'chocolate',
+			'lemon': 'lemon'
+		}
+	];
+
+// Initialize cookie key variables
+	var sugar = 'sugar';
+	var chocolate = 'chocolate';
+	var lemon = 'lemon';
+
+// Sign in and assign cookie key variables
+	$('#sign-in').click(function () {
+		if ($('#username').val() === 'Cameron') {
+			sugar = users[0].sugar;
+			chocolate = users[0].chocolate;
+			lemon = users[0].lemon;
+		} else if ($('#username').val() === 'Casey') {
+			sugar = users[1].sugar;
+			chocolate = users[1].chocolate;
+			lemon = users[1].lemon;
+		} else {
+			sugar = users[2].sugar;
+			chocolate = users[2].chocolate;
+			lemon = users[2].lemon;
+		}
+	})
 
 
-
-		$('#sugar').html(Cookies.get('sugar'));
-		$('#chocolate').html(Cookies.get('chocolate'));
-		$('#lemon').html(Cookies.get('lemon'));
+	$('#sugar').html(Cookies.get(sugar));
+	$('#chocolate').html(Cookies.get(chocolate));
+	$('#lemon').html(Cookies.get(lemon));
 	
 
 	
 	$('#sugar-quantity').click(function () {
-		Cookies.set('sugar', $(this).val());
-		$('#sugar').html(Cookies.get('sugar'));
+		Cookies.set(sugar, $(this).val());
+		$('#sugar').html(Cookies.get(sugar));
 	})
 	$('#chocolate-quantity').click(function () {
-		Cookies.set('chocolate', $(this).val());
-		$('#chocolate').html(Cookies.get('chocolate'));
+		Cookies.set(chocolate, $(this).val());
+		$('#chocolate').html(Cookies.get(chocolate));
 	})
 	$('#lemon-quantity').click(function () {
-		Cookies.set('lemon', $(this).val());
-		$('#lemon').html(Cookies.get('lemon'));
+		Cookies.set(lemon, $(this).val());
+		$('#lemon').html(Cookies.get(lemon));
 	})
 
 
